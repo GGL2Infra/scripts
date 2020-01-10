95129fae9445db926a0836f3fa21126f542f0864
nbanzicGG <59729855+nbanzicGG@users.noreply.github.com>
2020-01-10 12:00:15 +0100

    Add files via upload

---
+++ UnpinMailFromTaskbar.ps1
@@ -0,0 +1,11 @@
+﻿$registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband\AuxilliaryPins"
+$Name = "MailPin"
+$value = "1"
+IF(!(Test-Path $registryPath))
+{
+New-Item -Path $registryPath -Force | Out-Null
+New-ItemProperty -Path $registryPath -Name $name -Value $value `
+-PropertyType DWORD -Force | Out-Null}
+ELSE {
+New-ItemProperty -Path $registryPath -Name $name -Value $value `
+-PropertyType DWORD -Force | Out-Null}
